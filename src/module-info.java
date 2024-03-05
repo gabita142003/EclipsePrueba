b816bac624c7edb3fba00119c344988c5ad4ea40
gabita142003 <gabita142003@gmail>
2024-03-04 18:23:54 -0600

    primer cambio

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module pruebaGIT_ {
+}
---
+++ src/pruebaGIT_/EjemploGIT.java
@@ -0,0 +1,10 @@
+package pruebaGIT_;
+
+public class EjemploGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println ("Hola estoy probando GIT");
+	}
+
+}
